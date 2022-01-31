d1866c27d69ec7a7af9760a05590d8a88fc030c3
Danjb <danielkrns@gmail.com>
2022-01-31 23:40:40 +0200

    updated instructions, added an easier way to compile files

---
+++ compile_project.bat
@@ -0,0 +1,2 @@
+cmd /c "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\vsdevcmd.bat"
+cmd /c "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30037\bin\Hostx86\x86\cl.exe" /Zi /EHsc /nologo /Fe olcExampleProgram.cpp
